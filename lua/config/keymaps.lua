dab4edb2c59d374bc57865c23915e80458bd7441
David Rivero <davidmriverog@gmail.com>
2025-11-16 10:44:43 -0300

    fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,17 +4,9 @@
 local wk = require("which-key")
 local map = LazyVim.safe_keymap_set
 
+-- groups custom keymaps
 wk.add({
-  { "<leader>m", group = "+Visual Multi" }, -- group
-  { "<leader>ma", "<Plug>(VM-Select-All)<Tab>", desc = "Select All", mode = "n" },
-  { "<leader>ma", "<Plug>(VM-Select-All)<Tab>", desc = "Select All", mode = "n" },
-  { "<leader>mr", "<Plug>(VM-Start-Regex-Search)", desc = "Start Regex Search", mode = "n" },
-  { "<leader>mp", "<Plug>(VM-Add-Cursor-At-Pos)", desc = "Add Cursor At Pos", mode = "n" },
-  { "<leader>mo", "<Plug>(VM-Toggle-Mappings)", desc = "Toggle Mapping", mode = "n" },
-})
-
-wk.add({
-  { "<leader>T", group = "+Tools Compa" },
+  { "<leader>T", group = "+Tools" },
   {
     "<leader>Tn",
     function()
@@ -26,5 +18,16 @@ wk.add({
 })
 
 -- override keymaps
-map("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "+Move Down" })
-map("n", "<A-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "+Move Up" })
+
+local opts = { noremap = true, silent = true }
+-- Normal-mode commands
+map("n", "<A-Down>", ":MoveLine 1<CR>", opts)
+map("n", "<A-Up>", ":MoveLine -1<CR>", opts)
+map("n", "<A-S-Left>", ":MoveWord -1<CR>", opts)
+map("n", "<A-S-Right>", ":MoveWord 1<CR>", opts)
+
+-- Visual-mode commands
+map("x", "<A-Down>", ":MoveBlock 1<CR>", opts)
+map("x", "<A-Up>", ":MoveBlock -1<CR>", opts)
+map("v", "<A-Left>", ":MoveHBlock -1<CR>", opts)
+map("v", "<A-Right>", ":MoveHBlock 1<CR>", opts)
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,8 +21,6 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "plugins" },
   },
   defaults = {
