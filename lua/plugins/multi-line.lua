ce66a6df7604f8ebd389b9ff767c34b86937a6ac
David Rivero <davidmriverog@gmail.com>
2025-12-08 17:55:12 -0300

    fixes 2

--- lua/plugins/multi-line.lua
+++ lua/plugins/multi-line.lua
@@ -2,26 +2,17 @@
 
 return {
   {
-    -- Plugin: vim-multiple-cursors
-    -- URL: https://github.com/terryma/vim-multiple-cursors
-    -- Description: A Vim plugin that allows multiple cursors for simultaneous editing.
-    "terryma/vim-multiple-cursors",
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+    init = function()
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_maps = {
+        ["Find Under"] = "",
+      }
+      vim.g.VM_add_cursor_at_pos_no_mappings = 1
+    end,
   },
   {
     "hinell/move.nvim",
-  },
-  {
-    "brenton-leighton/multiple-cursors.nvim",
-    version = "*", -- Use the latest tagged version
-    opts = {}, -- This causes the plugin setup function to be called
-    keys = {
-      { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
-      { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
-
-      { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-      { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
-
-      { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or remove cursor" },
-    },
   },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,11 +2,23 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    cmd = { "Neotree" },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true,
+          },
+        },
+      })
+    end,
   },
   {
     "antosha417/nvim-lsp-file-operations",
