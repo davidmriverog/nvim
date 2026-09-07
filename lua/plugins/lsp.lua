26b300db6afabd7bc7299ee3b69f39d4d6d3d06d
David Rivero <davidmnriverog@gmail.com>
2026-09-07 19:13:27 -0300

    feat: changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -137,12 +137,6 @@ return {
     },
   },
 
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.formatting.biome" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
